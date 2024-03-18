e7fada89901526e5ccccf680684927e94f1ca913
Manfredas Lamsargis <manfredas.lamsargis@proton.me>
2024-03-18 14:48:09 +0200

    init

---
+++ Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Flare;
+
+
+// git submodule update --init --recursive (REMEMBER TO UPDATE)
+namespace Console
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+            Client client = new Client();
+            
+            await client.ConnectToServer();
+
+            // TODO - challenge issuence, using Isopoh.Cryptography.Argon2 lib
+
+            if (client.IsConnected)
+                System.Console.WriteLine("Connected to server");
+        }
+    }
+}
