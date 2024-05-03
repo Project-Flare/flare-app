47e929744acb0562673f9aa333bae3a88a7a662b
Manfredas Lamsargis <manfredas.lamsargis@proton.me>
2024-05-03 23:00:50 +0300

    dev: testing services, both do work as intended.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,12 +10,14 @@ namespace ConsoleApp
 			var clientManager = new ClientManager("https://rpc.f2.project-flare.net");
 			clientManager.Credentials.Username = "testing_user_0";
 			clientManager.Credentials.Password = "190438934";
+			//clientManager.HashPassword();
 			clientManager.Credentials.Argon2Hash = "$argon2i$v=19$m=524288,t=3,p=4$dGVzdGluZ191c2VyXzBwcm9qZWN0LWZsYXJlLm5ldGVNVEhJaWl0NlNTcWZKdWg2UEovM3c$tHhA3AmlEH8ao3vypVV36eyzbKfuX2b5a+5OCdD0kJI";
 			await clientManager.LoginToServerAsync();
-			var messagingSendingService = new MessagingSendingService(new Process<MSSState, MSSCommand>(MSSState.Connected), "https://rpc.f2.project-flare.net");
-			messagingSendingService.RunService(clientManager.channel);
-			messagingSendingService.SendMessage(new MessagingSendingService.Message("testing_user_0", "HEELOO THERE", clientManager.Credentials.AuthToken));
-
+			var messageSendingService = new MessagingSendingService(new Process<MSSState, MSSCommand>(MSSState.Connected), "https://rpc.f2.project-flare.net", clientManager.Credentials.AuthToken, clientManager.channel);
+			messageSendingService.StartService();
+			messageSendingService.SendMessage(new MessagingSendingService.Message("testing_user_0", "HEELOO THERE"));
+			var messageReceivingService = new MessageReceivingService(new Process<MRSState, MRSCommand>(MRSState.Connecting), "wss://ws.f2.project-flare.net/", clientManager.Credentials.AuthToken);
+			messageReceivingService.StartService();
 			Thread.Sleep(8000000);
 		}
 	}
